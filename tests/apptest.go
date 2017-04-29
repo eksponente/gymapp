6d0f5b7746b58f0fa592848ccf305e4387892248
Rugile Pevceviciute <rugilena@gmail.com>
2017-04-29 18:13:12 +0100

    Add database test

--- tests/apptest.go
+++ tests/apptest.go
@@ -1,7 +1,12 @@
 package tests
 
 import (
+	"fmt"
+
+	db "github.com/revel/modules/db/app"
 	"github.com/revel/revel/testing"
+
+	r "github.com/revel/revel"
 )
 
 type AppTest struct {
@@ -16,6 +21,11 @@ func (t *AppTest) TestThatIndexPageWorks() {
 	t.Get("/")
 	t.AssertOk()
 	t.AssertContentType("text/html; charset=utf-8")
+}
+
+func (t *AppTest) TestDatabaseConnection() {
+	fmt.Print(r.Config.String("db.spec"))
+	db.Init()
 }
 
 func (t *AppTest) After() {
