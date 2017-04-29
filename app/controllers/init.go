b924cd09d0e48bc95278616dacf64c8b7de33093
Rugile Pevceviciute <rugilena@gmail.com>
2017-04-29 18:41:46 +0100

    Enable init db

--- app/controllers/init.go
+++ app/controllers/init.go
@@ -3,7 +3,7 @@ package controllers
 import "github.com/revel/revel"
 
 func init() {
-	// revel.OnAppStart(InitDB)
+	revel.OnAppStart(InitDB)
 	revel.InterceptMethod((*GorpController).Begin, revel.BEFORE)
 	revel.InterceptMethod((*GorpController).Commit, revel.AFTER)
 	revel.InterceptMethod((*GorpController).Rollback, revel.FINALLY)
