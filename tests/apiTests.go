e9e10456764c3c9c12148aafc41e75b9b0fffef8
Rugile Pevceviciute <rugilena@gmail.com>
2017-05-02 20:00:10 +0100

    Refactor authentication

---
+++ tests/apiTests.go
@@ -0,0 +1,34 @@
+package tests
+
+// import (
+// 	"github.com/revel/revel/testing"
+// )
+//
+// type ApiTest struct {
+// 	testing.TestSuite
+// }
+//
+// func (t *ApiTest) Before() {
+// 	println("Set up")
+//
+// }
+//
+// func (t *ApiTest) TestThatIndexPageWorks() {
+// 	t.Get("/")
+// 	t.AssertOk()
+// 	t.AssertContentType("text/html; charset=utf-8")
+// }
+//
+// func (t *ApiTest) TestCreatingNewUser() {
+//     data := url.Values{
+//         "email": "example@user.com"
+//         "name": "Vardenis Pavardenis"
+//         "password": "kcromuva"
+//     }
+//     t.PostForm("/user/create", data)
+//
+// }
+//
+// func (t *ApiTest) After() {
+// 	println("Tear down")
+// }
