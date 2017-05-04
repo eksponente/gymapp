cdf7b1ba9de93741bc859b6ea324d352be9506ad
Rugile Pevceviciute <rugilena@gmail.com>
2017-05-04 18:50:58 +0100

    Add tests

--- tests/apiTests.go
+++ tests/apiTests.go
@@ -1,8 +1,8 @@
 package tests
 
 import (
+	"encoding/json"
 	"errors"
-	"fmt"
 	"gymapp/app/controllers"
 	"gymapp/app/models"
 	"net/url"
@@ -33,7 +33,7 @@ func (t *ApiTest) Before() {
 func (t *ApiTest) TestCreatingNewUser() {
 	data := url.Values{}
 	data.Set("email", "1@test.com")
-	data.Set("name", "Vardenis Pavardenis")
+	data.Set("name", "Vardenis Pa-Vardenis")
 	data.Set("password", "slaptazodis")
 	t.PostForm("/user/create", data)
 	t.AssertStatus(201)
@@ -42,7 +42,7 @@ func (t *ApiTest) TestCreatingNewUser() {
 	controllers.Dbm.SelectOne(&user, "SELECT * FROM users WHERE Email=$1", "1@test.com")
 	t.AssertEqual("1@test.com", user.Email)
 	t.AssertEqual(false, user.IsSuperuser)
-	t.AssertEqual("Vardenis Pavardenis", user.Name)
+	t.AssertEqual("Vardenis Pa-Vardenis", user.Name)
 }
 
 func (t *ApiTest) TestUniqueEmailsOnly() {
@@ -52,7 +52,64 @@ func (t *ApiTest) TestUniqueEmailsOnly() {
 	data.Set("password", "slaptazodis")
 	t.PostForm("/user/create", data)
 	t.AssertStatus(400)
-	fmt.Println(t.Response.Body)
+	var res map[string]interface{}
+	json.Unmarshal(t.ResponseBody, &res)
+	t.AssertEqual("User with that email already exists.", res["error"].(string))
+
+}
+
+func (t *ApiTest) TestValidEmailsOnly() {
+	data := url.Values{}
+	data.Set("email", "rugilena@gmail")
+	data.Set("name", "Vardenis Pavardenis")
+	data.Set("password", "slaptazodis")
+	t.PostForm("/user/create", data)
+	t.AssertStatus(400)
+
+	var user models.User
+	err := controllers.Dbm.SelectOne(&user, "SELECT * FROM users WHERE Email=$1", "rugilena@gmail")
+	if err == nil {
+		panic(errors.New("New user has been created."))
+	}
+
+	t.AssertContains("Invalid email.")
+
+	// t.AssertEqual(res["errors"].([]interface{})[0].(map[string]interface{})["Message"].(string), "Invalid email.")
+}
+
+func (t *ApiTest) TestValidNamesOnly() {
+	data := url.Values{}
+	data.Set("email", "2@test.com")
+	data.Set("name", "()()()")
+	data.Set("password", "slaptazodis")
+	t.PostForm("/user/create", data)
+	t.AssertStatus(400)
+
+	var user models.User
+	err := controllers.Dbm.SelectOne(&user, "SELECT * FROM users WHERE Email=$1", "2@test.com")
+	if err == nil {
+		panic(errors.New("New user has been created."))
+	}
+
+	// t.AssertEqual(res["errors"].([]interface{})[0].(map[string]interface{})["Message"].(string), "Valid name is required.")
+	t.AssertContains("Valid name is required.")
+}
+
+func (t *ApiTest) TestValidPasswordsOnly() {
+	data := url.Values{}
+	data.Set("email", "3@test.com")
+	data.Set("name", "Vardas")
+	data.Set("password", "sla")
+	t.PostForm("/user/create", data)
+	t.AssertStatus(400)
+
+	var user models.User
+	err := controllers.Dbm.SelectOne(&user, "SELECT * FROM users WHERE Email=$1", "3@test.com")
+	if err == nil {
+		panic(errors.New("New user has been created."))
+	}
+
+	t.AssertContains("Minimum size is 8")
 }
 
 func (t *ApiTest) After() {
