37faad4debe71ab6cacf6c52b173457a9a39f3d0
Rugile Pevceviciute <rugilena@gmail.com>
2017-05-12 18:16:15 +0100

    add exp date explanation

--- app/models/token.go
+++ app/models/token.go
@@ -3,5 +3,5 @@ package models
 type Token struct {
 	UserID         int    `db:"user_id"`
 	Token          string `db:"token"`
-	ExpirationDate string
+	ExpirationDate string `db:"expirationdate"`
 }
